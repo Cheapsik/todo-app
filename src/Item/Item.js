68072255627e51472845c24cab953f9077fa664a
Łukasz Fijał <cheapsik@gmail.com>
2021-04-17 20:04:24 +0200

    feat(Item): adding "done" button and styles for him. After click, task is crossed out and button disapear

--- src/Item/Item.js
+++ src/Item/Item.js
@@ -1,18 +1,33 @@
+import React, { useState } from "react";
 import "./Item.scss";
-import { MdDeleteForever as RemoveItem } from "react-icons/md";
+import { MdDeleteForever as RemoveItem, MdDone as Done } from "react-icons/md";
 
-const Item = ({ item, handleDeleteTask,id }) => {
-  const handleButtonClick = () => {
-    handleDeleteTask(item,id);
+const Item = ({ item, handleDeleteTask, id }) => {
+  let [done, setDone] = useState(false);
+
+  const handleDoneTask = () => {
+    if (!done) setDone((done = true));
+  };
+
+  const handleDelTask = () => {
+    handleDeleteTask(item, id);
   };
 
   return (
     <>
-      <li>
+      <li style={{ textDecoration: done ? "line-through" : "none" }}>
         {item}
-        <button onClick={handleButtonClick}>
-          <RemoveItem />
-        </button>
+        <div className="icon-container">
+          <button
+            style={{ display: done ? "none" : "block" }}
+            onClick={handleDoneTask}
+          >
+            <Done />
+          </button>
+          <button onClick={handleDelTask}>
+            <RemoveItem />
+          </button>
+        </div>
       </li>
     </>
   );
